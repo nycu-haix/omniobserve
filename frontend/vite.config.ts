9aaa799b7c0af62728a2d1579f7242a9ced69b7a
zardfans113 <jasontw77661@gmail.com>
2026-04-30 22:12:33 +0800

    remove "pnpm dev" test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,5 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "https://api.omni.elvismao.com",
-				changeOrigin: true
-			}
-		}
-	}
+	plugins: [react()]
 });
