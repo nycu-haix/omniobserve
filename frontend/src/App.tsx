9a1087a30fc2bc130f48cc9bf7065ebea45f7a3d
Sky Hong <sky9109@gmail.com>
2026-05-01 21:44:08 +0800

    feat: add HomePage for meeting URL generation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
 import "./App.css";
+import { HomePage } from "./components/HomePage";
 import MeetingRoom from "./components/MeetingRoom";
 
 function App() {
-	return <MeetingRoom />;
+	const params = new URLSearchParams(window.location.search);
+	const shouldOpenMeeting = params.has("room_name") || params.has("id") || params.has("name");
+
+	return shouldOpenMeeting ? <MeetingRoom /> : <HomePage />;
 }
 
 export default App;
